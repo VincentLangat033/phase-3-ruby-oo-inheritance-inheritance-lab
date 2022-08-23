653bd1327ef06237ace4afe800027e969b7205fc
Vincent Langat <kimutailangat033@gmail.com>
2022-08-23 05:47:39 +0300

    commit tree 7745f0261570cc8d261fd840d68d78a57c90633e

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative './user'
+class Student < User
+    def initialize
+    @knowledge = []
+    end
+    def learn (string_learn)
+        @knowledge << string_learn
+
+    end
+    def knowledge
+        @knowledge
+    end
 
 end
